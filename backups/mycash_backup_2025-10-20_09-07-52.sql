89ac96dcd95e3a8977117a03c46639ead25afccf
paulderire <hpaulderiree@gmail.com>
2025-10-20 13:52:16 +0200

    another version

--- backups/mycash_backup_2025-10-20_09-07-52.sql
+++
@@ -1,1 +0,0 @@
-mysqldump.exe: Got error: 1045: "Access denied for user 'root'@'localhost' (using password: YES)" when trying to connect
--- mycash-laravel-stub/resources/views/dashboard/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Dashboard</h1>
-<p>Welcome to your dashboard.</p>
-@endsection
